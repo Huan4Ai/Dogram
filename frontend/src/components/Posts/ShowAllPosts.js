216d4ba8785e123a1c109d5a80983ebd748e175e
Huan4Ai <aihuan96@gmail.com>
2021-11-17 11:51:39 -0500

    before wc

--- frontend/src/components/Posts/ShowAllPosts.js
+++ frontend/src/components/Posts/ShowAllPosts.js
@@ -1,11 +1,31 @@
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { getPosts } from "../../store/post";
 
 function ShowAllPosts() {
+  const dispatch = useDispatch();
+  const posts = useSelector(state => state.post);
+
+  useEffect(() => {
+    dispatch(getPosts())
+  }, [dispatch]);
 
 
-  return null;
+
+  return (
+    <div>
+      {Object.keys(posts).map(post =>
+        <div key={posts[post].id}>
+          <p>{posts[post].description}</p>
+        </div>
+
+
+      )}
+    </div>
+
+  );
 }
 
 export default ShowAllPosts;
