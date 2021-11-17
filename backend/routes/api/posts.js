c0c066355102644d00255e9258f6ce4ca36ca07b
Huan4Ai <aihuan96@gmail.com>
2021-11-17 17:22:13 -0500

    before aws react

--- backend/routes/api/posts.js
+++ backend/routes/api/posts.js
@@ -2,6 +2,8 @@ const express = require('express');
 const asyncHandler = require('express-async-handler');
 const { requireAuth } = require('../../utils/auth');
 const { User, Post } = require('../../db/models');
+import { singlePublicFileUpload } from '../../awsS3';
+import { singleMulterUpload } from '../../awsS3';
 
 const router = express.Router();
 
@@ -10,7 +12,17 @@ router.get('/', asyncHandler(async (req, res) => {
   return res.json(allPosts);
 }));
 
+router.post("/", singleMulterUpload("image"), asyncHandler(async (req, res) => {
+  const { description } = req.body;
+  const photo_url = await singlePublicFileUpload(req.file);
+  const newPost = await Post.create({
+    user_id: req.user.id,
+    description,
+    photo_url
+  });
+  res.json(newPost);
 
+}));
 
 
 
--- frontend/src/store/post.js
+++ frontend/src/store/post.js
@@ -1,11 +1,17 @@
 import { csrfFetch } from "./csrf";
 
 const LOAD_POSTS = 'posts/LOAD_POSTS';
+const ADD_POST = 'posts/ADD_POST';
 
 const loadPosts = (list) => ({
   type: LOAD_POSTS,
   list
-})
+});
+
+const addPost = (post) => ({
+  type: ADD_POST,
+  post
+});
 
 
 export const getPosts = () => async dispatch => {
@@ -17,11 +23,36 @@ export const getPosts = () => async dispatch => {
   }
 }
 
+export const createPost = (post) => async (dispatch) => {
+  const { user_id, description, image } = post;
+  const formData = new FormData();
+  formData.append("user_id", user_id);
+  formData.append("description", description);
+
+  if (image) formData.append("image", image);
+
+  const res = await csrfFetch('/api/posts/', {
+    method: "POST",
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+    body: formData,
+  });
+
+  const post = await res.json();
+  dispatch(addPost(post));
+
+
+
+};
+
 const postReducer = (state = {}, action) => {
   switch (action.type) {
     case LOAD_POSTS:
       return { ...state, ...action.list };
 
+    case ADD_POST:
+      return {...state, ...action.post}
 
 
     default:
