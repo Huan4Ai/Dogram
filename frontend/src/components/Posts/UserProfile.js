43b137922418fcc9d65abad3f9e8953349dd7a59
Huan4Ai <aihuan96@gmail.com>
2021-11-19 11:58:11 -0500

    created initial user profile page

--- frontend/src/components/Posts/UserProfile.js
+++ frontend/src/components/Posts/UserProfile.js
@@ -4,11 +4,14 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { getPosts } from "../../store/post";
+import "./UserProfile.css";
 
 function UserProfilePage() {
   const dispatch = useDispatch();
 
   const user = useSelector(state => state.session.user);
+  const posts = useSelector(state => state.post);
+
   const username = user.username;
   const userId = user.id;
 
@@ -16,10 +19,27 @@ function UserProfilePage() {
     dispatch(getPosts());
   }, [dispatch]);
 
+  const postLists = Object.values(posts);
+
+  const userPosts = postLists.filter(post => post.user_id == userId)
+  // console.log(userPosts)
+
+
   return (
-    <h2>
-      {username}
-    </h2>
+    <div>
+      <div className="userInfo">
+        <h2>{username}</h2>
+        <h4>{userPosts.length} Posts # Following # Followers</h4>
+      </div>
+      <div className="singlePost">
+        {userPosts.map(post =>
+          <div key={post.id}>
+            <img src={post.photo_url} alt="postImage" className="AllPostImages" />
+            <p>{post.description}</p>
+          </div>
+          )}
+      </div>
+    </div>
   )
 
 
