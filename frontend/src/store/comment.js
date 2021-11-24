717b0b131173ca2b8f65c51e996ffc85dc3a57eb
Huan4Ai <aihuan96@gmail.com>
2021-11-24 18:49:32 -0500

    added comment reducer

--- frontend/src/store/comment.js
+++ frontend/src/store/comment.js
@@ -5,9 +5,9 @@ const ADD_COMMENT = 'posts/ADD_COMMENT';
 const EDIT_COMMENT = 'posts/EDIT_COMMENT';
 const DELETE_COMMENT = 'posts/DELETE_COMMENT';
 
-const loadComments = (comments, postId) => ({
+const loadComments = (allComments, postId) => ({
   type: LOAD_COMMENTS,
-  comments,
+  allComments,
   postId
 });
 
@@ -26,4 +26,91 @@ const removeComment = (commentId) => ({
   commentId
 });
 
-export const getComments = (id) => 
+export const getComments = (id) => async (dispatch) => {
+  const response = await csrfFetch(`/api/posts/${id}/comments`);
+
+  if (response.ok) {
+    const allComments = await response.json();
+
+    dispatch(loadComments(allComments, id));
+  };
+
+
+};
+
+export const createComment = (data, postId) = async (dispatch) => {
+  const response = await csrfFetch(`/api/posts/${postId}/comments`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+
+  if (response.ok) {
+    const comment = await response.json();
+    dispatch(addComment(comment));
+    return comment;
+  }
+
+
+};
+
+export const updataComment = (data) => async (dispatch) => {
+  const response = await csrfFetch(`/api/comments/${data.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+
+  });
+
+  if (response.ok) {
+    const comment = await response.json();
+    dispatch(editComment(comment));
+    return comment;
+  }
+
+};
+
+export const deleteComment = (commentId) => async (dispatch) => {
+  const response = await csrfFetch(`/api/comments/${commentId}`, {
+    method: 'DELETE'
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(removeComment(data));
+  }
+
+};
+
+
+const commentReducer = (state = {}, action) => {
+
+  switch (action.type) {
+    case LOAD_COMMENTS:
+      let newComments = { ...state };
+      action.allComments.forEach((comment) => {
+        newComments[comment.id] = comment;
+      });
+      return newComments
+
+    case ADD_COMMENT:
+      return { ...state, [action.comment.id]: action.comment };
+
+    case EDIT_COMMENT:
+      return { ...state, [action.comment.id]: action.comment };
+
+    case DELETE_COMMENT:
+      const newState = { ...state };
+      delete newState[action.commentId.id];
+      return newState;
+
+    default:
+      return state
+  }
+}
+
+export default commentReducer;
