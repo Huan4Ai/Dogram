edee0943dc38cc97bc03a12166d32d845a5a424e
Huan4Ai <aihuan96@gmail.com>
2021-11-23 20:07:03 -0500

    added blue background image

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -5,7 +5,28 @@ const { User, Post, Comment } = require('../../db/models');
 
 const router = express.Router();
 
+router.put('/:id(\\d+)', requireAuth, asyncHandler(async (req, res) => {
 
+  const commentToChange = await Comment.findByPk(req.params.id);
+  await commentToChange.update(req.body);
+  return res.json(commentToChange);
+
+
+}));
+
+router.delete('/:id(\\d+)', requireAuth, asyncHandler(async (req, res) => {
+  const commentId = req.params.id;
+  const commentToDelete = await Comment.findByPk(req.params.id);
+
+  if (commentToDelete) {
+    await commentToDelete.destroy();
+    return res.json(commentId);
+  } else {
+    throw new Error('Comment not found');
+  };
+
+
+}));
 
 
 
---
+++ frontend/src/store/comment.js
@@ -0,0 +1,29 @@
+import { csrfFetch } from "./csrf";
+
+const LOAD_COMMENTS = 'posts/LOAD_COMMENTS';
+const ADD_COMMENT = 'posts/ADD_COMMENT';
+const EDIT_COMMENT = 'posts/EDIT_COMMENT';
+const DELETE_COMMENT = 'posts/DELETE_COMMENT';
+
+const loadComments = (comments, postId) => ({
+  type: LOAD_COMMENTS,
+  comments,
+  postId
+});
+
+const addComment = (comment) => ({
+  type: ADD_COMMENT,
+  comment
+});
+
+const editComment = (comment) => ({
+  type: EDIT_COMMENT,
+  comment
+});
+
+const removeComment = (commentId) => ({
+  type: DELETE_COMMENT,
+  commentId
+});
+
+
