81c1a1e4690413c06f5dd07517552eb7cd747757
Huan4Ai <aihuan96@gmail.com>
2021-11-29 21:58:52 -0500

    hide edit modal for other user

--- frontend/src/components/Comments/EditIndex.js
+++ frontend/src/components/Comments/EditIndex.js
@@ -2,20 +2,30 @@ import React from "react";
 import { useState } from "react";
 import EditCommentForm from "./EditComment";
 import { Modal } from "../../context/Modal"
+import { useSelector } from "react-redux";
 
 function EditCommentFormModal({ singleComment }) {
   const [showModal, setShowModal] = useState(false);
 
-  return (
-    <>
-      <i className="far fa-edit" onClick={() => setShowModal(true)}></i>
-      {showModal && (
-        <Modal onClose={() => setShowModal(false)}>
-          <EditCommentForm onClose={() => setShowModal(false)} singleComment={singleComment} />
-        </Modal>
-      )}
-    </>
-  );
+  const modelUserId = singleComment?.user_id;
+  const currentUserId = useSelector(state => state.session.user.id);
+
+  if (modelUserId === currentUserId) {
+
+    return (
+      <>
+        <i className="far fa-edit" onClick={() => setShowModal(true)}></i>
+        {showModal && (
+          <Modal onClose={() => setShowModal(false)}>
+            <EditCommentForm onClose={() => setShowModal(false)} singleComment={singleComment} />
+          </Modal>
+        )}
+      </>
+    );
+
+  }
+
+  return null;
 }
 
 export default EditCommentFormModal;
