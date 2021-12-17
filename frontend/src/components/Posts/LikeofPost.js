b6ed4bb4864dad4d2d574db41bd35f31219df0e1
Huan4Ai <aihuan96@gmail.com>
2021-12-17 00:16:36 -0500

    isliked ternary statement

--- frontend/src/components/Posts/LikeofPost.js
+++ frontend/src/components/Posts/LikeofPost.js
@@ -3,19 +3,45 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import "./Like.css"
 
-function LikeAPost({post}) {
+function LikeAPost({ post }) {
 
-  console.log(post.Likes.length)
+  const user = useSelector(state => state.session.user);
+  const userId = user.id;
 
   let numOfLikes = post?.Likes?.length
+
+
+
+  const isLiked = () => {
+    let likes = post.Likes;
+
+    if (likes) {
+      for (let i = 0; i < likes.length; i++) {
+        if (likes[i].user_id === userId) {
+          return true
+        }
+      }
+    }
+    return false;
+  }
+
   return (
     <div className="likeContainer">
-      <div className="likeIcon">
+      {/* <div className="likeIcon">
         <i class="far fa-heart"></i>
-      </div>
-      <div className="likeIcon-Red">
+      </div> */}
+      {/* <div className="likeIcon-Red">
         <i class="fas fa-heart"></i>
-      </div>
+      </div> */}
+      {isLiked ? (
+        <div className="likeIcon-Red">
+          <i className="fas fa-heart"></i>
+        </div>
+      ) : (
+          <div className="likeIcon">
+            <i className="far fa-heart"></i>
+          </div>
+      )}
       <p className="likeCount">{numOfLikes} {numOfLikes <= 1 ? "like" : "likes"}</p>
     </div>
   )
