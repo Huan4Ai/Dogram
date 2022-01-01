10c1e93f4e130942e0d6ff9a325f5c718b68a83d
Huan4Ai <aihuan96@gmail.com>
2022-01-01 15:28:37 -0500

    took off isPrivate in model

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -5,8 +5,8 @@ const bcrypt = require("bcryptjs");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     toSafeObject() {
-      const { id, username, email, profilePicture, about, isPrivate } = this; // context will be the User instance
-      return { id, username, email, profilePicture, about, isPrivate };
+      const { id, username, email, profilePicture, about } = this; // context will be the User instance
+      return { id, username, email, profilePicture, about };
     }
     validatePassword(password) {
       return bcrypt.compareSync(password, this.hashedPassword.toString());
