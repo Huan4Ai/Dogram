6dc9ac7373fb4726bc120a1030524447eec400d4
Huan4Ai <aihuan96@gmail.com>
2022-01-06 15:58:50 -0500

    copied edit comment modal to edit post

--- frontend/src/components/Posts/EditPost.js
+++ frontend/src/components/Posts/EditPost.js
@@ -3,13 +3,14 @@ import { useDispatch } from "react-redux";
 import { updatePost } from "../../store/post";
 import { useEffect } from "react";
 import { getSinglePost } from "../../store/post";
-import "./EditPost.css"
+import "./EditPost.css";
 
 function EditPost({ post, onClose }) {
   const dispatch = useDispatch();
   const id = post.id;
   const user_id = post.user_id;
   const photo_url = post.photo_url;
+  const username = post?.User.username;
 
   const [description, setDescription] = useState(post.description);
 
@@ -35,29 +36,42 @@ function EditPost({ post, onClose }) {
 
   };
 
+  const handleCancelClick = (e) => {
+    e.preventDefault();
+    onClose();
+  };
+
   useEffect(() => {
     dispatch(getSinglePost(post?.id))
-  }, [dispatch, post]);
-
-
+  }, [dispatch, post?.id]);
 
 
   return (
 
-    <div className="editPostContainer">
-      <h3>Edit a post</h3>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor='edit'>Edit description:</label>
-        </div>
-        <div>
-          <input id='edit' type="text" value={description} onChange={(e) => setDescription(e.target.value)} required />
-        </div>
-        <div>
-          <button type="submit" id="editPostButton">Edit</button>
-        </div>
-      </form>
-    </div>
+    // <div className="editPostContainer">
+    //   <h3>Edit a post</h3>
+    //   <form onSubmit={handleSubmit}>
+    //     <div>
+    //       <label htmlFor='edit'>Edit description:</label>
+    //     </div>
+    //     <div>
+    //       <input id='edit' type="text" value={description} onChange={(e) => setDescription(e.target.value)} required />
+    //     </div>
+    //     <div>
+    //       <button type="submit" id="editPostButton">Edit</button>
+    //     </div>
+    //   </form>
+    // </div>
+    <form onSubmit={handleSubmit} className="editCommentContainer">
+      <p id="usernameTitle">{username}</p>
+      <div>
+        <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} className="editCommentInput" required />
+      </div>
+      <div className="editCommentButtons">
+        <button type="submit" id="submitButtonModal">Update post</button>
+        <button type="button" onClick={handleCancelClick} id="cancelButtonModal">Cancel</button>
+      </div>
+    </form>
   )
 }
 
