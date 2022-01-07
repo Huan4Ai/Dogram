bb71b62534d8a5c9c19abd2e3922198c92d67629
Huan4Ai <aihuan96@gmail.com>
2022-01-07 15:25:33 -0500

    before adding delete confirmation

--- frontend/src/components/Posts/SinglePost.js
+++ frontend/src/components/Posts/SinglePost.js
@@ -9,6 +9,7 @@ import SinglePostLike from "./SinglePostLike";
 import AddCommentSinglePost from "./AddCommentSingle";
 import EditPostFormModal from "./EditPostIndex";
 import DeletePost from "./DeletePost";
+import { Link } from "react-router-dom";
 
 function SinglePost() {
   const dispatch = useDispatch();
@@ -32,7 +33,9 @@ function SinglePost() {
         <div className="postOwnerInfo">
           <>
             <img src={SinglePost?.User?.profilePicture} alt="profilePicture" className="profilePictureOnPostcard" />
-            <p className="usernameOnPostcard">{SinglePost?.User?.username}</p>
+            <Link className="usernameOnPostcard" to={`/users/${SinglePost?.User?.id}`}>
+              <p>{SinglePost?.User?.username}</p>
+            </Link>
           </>
           {SinglePost?.User?.id === currentUserId &&
             <div className="editAndDeleteIcons">
@@ -44,13 +47,17 @@ function SinglePost() {
         <div className="postcardComment">
           <div className="postOwnerInfo">
             <img src={SinglePost?.User?.profilePicture} alt="profilePicture" className="profilePictureOnPostcard" />
-            <p className="usernameOnPostcard">{SinglePost?.User?.username}</p>
+            <Link className="usernameOnPostcard" to={`/users/${SinglePost?.User?.id}`}>
+              <p>{SinglePost?.User?.username}</p>
+            </Link>
             <p className="postDescriptonOnPostcard">{SinglePost?.description}</p>
           </div>
           {SinglePost?.Comments?.map((comment, index) =>
             <div key={index} className="postOwnerInfo">
               <img src={comment?.User?.profilePicture} alt="profilePicture" className="profilePictureOnPostcard" />
-              <p className="usernameOnPostcard">{comment?.User?.username}</p>
+              <Link className="usernameOnPostcard" to={`/users/${comment?.User?.id}`}>
+                <p>{comment?.User?.username}</p>
+              </Link>
               <p className="postDescriptonOnPostcard">{comment?.content}</p>
             </div>
           )}
