688d7d7997a617f785770d17625b4ef4608af800
Huan4Ai <aihuan96@gmail.com>
2022-01-07 21:07:59 -0500

    styled delete comment modal

--- frontend/src/components/Comments/DeleteComment.js
+++ frontend/src/components/Comments/DeleteComment.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteComment } from "../../store/comment";
+import "./DeleteIndex.css";
 
-function DeleteComment({ singleComment }) {
+
+function DeleteComment({ singleComment, onClose }) {
 
   const dispatch = useDispatch();
-
-  const userIdPassed = singleComment.user_id;
-  const currentUserId = useSelector((state) => state.session.user.id);
 
 
   const handleDelete = (e) => {
@@ -15,16 +14,22 @@ function DeleteComment({ singleComment }) {
     dispatch(deleteComment(singleComment?.id))
   }
 
-  if (userIdPassed === currentUserId) {
-    return (
-      <>
-        <i className="far fa-trash-alt" onClick={handleDelete}></i>
-      </>
-  )
+  const handleCancelClick = (e) => {
+    e.preventDefault();
+    onClose();
+  };
 
-  } else {
-    return null
-  }
+
+  return (
+    <div className="deleteModalContainer">
+      <div className="delete-top">
+        <p className="del-title">Delete Comment?</p>
+        <p className="del-desc">Are you sure you want to delete this comment?</p>
+      </div>
+      <div className="delete-comment-button" onClick={handleDelete}>Delete</div>
+      <div className="cancel-delete-comment" onClick={handleCancelClick}>Cancel</div>
+    </div>
+  )
 
 
 
---
+++ frontend/src/components/Comments/DeleteIndex.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { useState } from "react";
+import DeleteComment from "./DeleteComment";
+import { Modal } from "../../context/Modal";
+import { useSelector } from "react-redux";
+
+
+function DeleteCommentModal({singleComment}) {
+
+  const [showModal, setShowModal] = useState(false);
+
+  const modelUserId = singleComment?.user_id;
+  const currentUserId = useSelector(state => state.session.user.id);
+
+  if (modelUserId === currentUserId) {
+
+    return (
+      <>
+        <i className="far fa-trash-alt" onClick={() => setShowModal(true)}></i>
+        {showModal && (
+          <Modal onClose={() => setShowModal(false)}>
+            <DeleteComment onClose={() => setShowModal(false)} singleComment={singleComment} />
+          </Modal>
+        )}
+      </>
+    );
+
+  }
+
+  return null;
+
+
+}
+
+export default DeleteCommentModal;
