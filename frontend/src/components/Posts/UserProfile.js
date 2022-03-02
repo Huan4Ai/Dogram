9b3560a0161e7099d63fa39059e8eab13856515d
Huan4Ai <aihuan96@gmail.com>
2022-03-01 21:59:39 -0500

    loaded followers on profile page

--- frontend/src/components/Posts/UserProfile.js
+++ frontend/src/components/Posts/UserProfile.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import { getPosts } from "../../store/post";
 import "./UserProfile.css";
 import { Link } from "react-router-dom";
-
+import { getSingleUser } from "../../store/user";
 
 function UserProfilePage() {
   const dispatch = useDispatch();
 
-  const user = useSelector(state => state.session.user);
-  const posts = useSelector(state => state.post);
+  const user = useSelector((state) => state.session.user);
+  const posts = useSelector((state) => state.post);
+  const numOfFollowers = useSelector(
+    (state) => state?.userReducer?.followers?.length
+  );
+  const numOfFollowing = useSelector(
+    (state) => state?.userReducer?.following?.length
+  );
 
   const username = user.username;
   const userId = user.id;
@@ -19,10 +25,16 @@ function UserProfilePage() {
     dispatch(getPosts());
   }, [dispatch]);
 
+  useEffect(() => {
+    dispatch(getSingleUser(userId));
+  }, [dispatch, userId]);
 
   const postLists = Object.values(posts);
 
-  const userPosts = postLists.filter(post => post.user_id === userId)
+  const userPosts = postLists.filter((post) => post.user_id === userId);
+
+  const numOfPosts = userPosts.length;
+
 
   return (
     <div className="myProfilePageWrapper">
@@ -30,26 +42,25 @@ function UserProfilePage() {
         <img src={user.profilePicture} className="profileP" alt="userProfile" />
         <div className="profileRight">
           <h2 className="user-info">{username}</h2>
+          <p>{numOfPosts} posts  {numOfFollowers} followers {numOfFollowing} following</p>
           <p className="about">{user.about}</p>
         </div>
       </div>
-      {/* <div>
-        <p>haha</p>
-      </div> */}
       <div className="user_posts_wrapper">
-        {userPosts.map(post =>
+        {userPosts.map((post) => (
           <div key={post.id} className="post_image_wrapper">
             <Link to={`/posts/${post?.id}`}>
-              <img src={post.photo_url} alt="postImage" className="postImageProfilePage" />
+              <img
+                src={post.photo_url}
+                alt="postImage"
+                className="postImageProfilePage"
+              />
             </Link>
           </div>
-        )}
+        ))}
       </div>
     </div>
-  )
-
-
-
-};
+  );
+}
 
 export default UserProfilePage;
